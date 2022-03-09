e9f9f03673f2f69ef19f460aa9820e92231b3fd2
Automatic Deployment (GitHub Action) <noreply@lanl.gov>
2022-03-09 15:52:33 +0000

    Documentation Update

--- doxygen/search/classes_e.js
+++ doxygen/search/classes_e.js
@@ -1,7 +1,7 @@
 var searchData=
 [
-  ['randomaccessmemory_882',['RandomAccessMemory',['../structCabana_1_1RandomAccessMemory.html',1,'Cabana']]],
-  ['referenceconjugategradient_883',['ReferenceConjugateGradient',['../classCajita_1_1ReferenceConjugateGradient.html',1,'Cajita']]],
-  ['referencestructuredsolver_884',['ReferenceStructuredSolver',['../classCajita_1_1ReferenceStructuredSolver.html',1,'Cajita']]],
-  ['replace_885',['Replace',['../structCajita_1_1ScatterReduce_1_1Replace.html',1,'Cajita::ScatterReduce']]]
+  ['randomaccessmemory_899',['RandomAccessMemory',['../structCabana_1_1RandomAccessMemory.html',1,'Cabana']]],
+  ['referenceconjugategradient_900',['ReferenceConjugateGradient',['../classCajita_1_1ReferenceConjugateGradient.html',1,'Cajita']]],
+  ['referencestructuredsolver_901',['ReferenceStructuredSolver',['../classCajita_1_1ReferenceStructuredSolver.html',1,'Cajita']]],
+  ['replace_902',['Replace',['../structCajita_1_1ScatterReduce_1_1Replace.html',1,'Cajita::ScatterReduce']]]
 ];
--- doxygen/search/namespaces_0.js
+++ doxygen/search/namespaces_0.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['cabana_948',['Cabana',['../namespaceCabana.html',1,'']]],
-  ['cajita_949',['Cajita',['../namespaceCajita.html',1,'']]]
+  ['cabana_965',['Cabana',['../namespaceCabana.html',1,'']]],
+  ['cajita_966',['Cajita',['../namespaceCajita.html',1,'']]]
 ];
